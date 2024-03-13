fea8a05ccd78383e5828fd86ac779d90d2b7acc7
Joshua Perry <josh@6bit.com>
2024-03-13 12:56:19 -0600

    So many unpushed updates

--- README.md
+++ README.md
@@ -1,78 +1,8 @@
 Couch Mail
 ===============
 
-I first played with CouchDB as I was evaluating nosql tech for a new project. 
-This was my first foray into nosql, I immediately fell for CouchDB's HTTP ethos.
+The brains behind a set of opinionated applications and configurations for
+deploying a multi-domain capable Postfix and Dovecot stack with user, alias,
+and seive scripts stored in CouchDB.
 
-In fact, it sent me off on my first exploration into functional programming with Erlang as I wanted to understand how it worked.
-
-About the same time I was also jonesing for a unified multi-domain mail exchange system for personal and small business use.
-
-I decided to use CouchDB to drive the realtime data persistence story of this stack, ipso facto the name.
-
-# Project
-
-This project is a set of opinionated applications and configurations for deploying a multi-domain capable Postfix and Dovecot stack with user, alias, and routing scripts stored in CouchDB.
-
-It at first began as a blog posts on ways describing ways to configure a mail exchange stack, that was a mistake.
-This is a complicated undertaking, it would be nice to get some help keeping it fresh and limber.
-
-As an output we want to provide one-button deployable, securely configured IMAP and SMTP services, with bad email mitigation (greylisting, trainable bayesian filtering, blacklists), and a user-scriptable routing engine in [Pigeonhole Sieve](https://wiki2.dovecot.org/Pigeonhole/Sieve).
-Just run the stack, then route IMAP and SMTP ports, and configure DNS.
-
-# Data Model
-
-## Domain
-
-## Mailbox
-
-## Alias
-
-## Sieve
-
-## Authentication
-
-Dovecot all the things!!
-
-# Data Flow
-
-## SMTP Receive
-
-## SMTP Client Send
-
-## IMAP Client
-
-## Filtering and Routing
-
-# Devops
-
-## Data Persistence
-
-## SMTP
-
-## Greylisting
-
-## SpamC
-
-## Blacklisting
-
-## IMAP
-
-## LDA
-
-## XMPP with Prosody
-
-## The Pile of TODOs
-
-- [ ] Arrange for the `/var/run/couchmail/` directory to exist before start
-- [ ] Kickass management UI that's not Futon
-- [ ] Add webmail client
-- [ ] Containerize, duh
-  - [ ] couchmail
-  - [ ] Dovecot
-  - [ ] Postfix
-  - [ ] spamassassin
-- [ ] Docker compose config
-- [ ] Automate PKI config with letsencrypt.org
-- [ ] Can we automate the DNS config in common deployment target environments?
-- [ ] Kubernetes configs
+It implements the Dovecot authentication protocol, which is widely used. 
--- app.js
+++ app.js
@@ -1,159 +1,243 @@
 const net = require('net'),
+      fs = require('fs'),
+      events = require('events'),
+      util = require('util'),
       carrier = require('carrier')
 
 const dbuser = process.env.COUCH_USER,
       dbpass = process.env.COUCH_PASSWORD,
-      dbhost = process.env.COUCH_HOST,
-      db = require('nano')(`http://${dbuser}:${dbpass}@${dbhost}:5984/mail`)
+      dbhost = process.env.COUCH_HOST
+      dburl = `http://${dbuser}:${dbpass}@${dbhost}:5984/mail`
+      db = require('nano')({
+        url: dburl,
+//        log: (id, args) => {
+//          console.log(id, args)
+//        },
+      })
 
-const postfixHandler = (serviceName, getHandler = () => Promise.reject()) => {
-  let clientId = 0
+// This function returns a generalized handler for a postfix request (domain, alias, or mailbox)
+let clientId = 0
+const postfixHandler = (serviceName, getHandler) => {
+  // Fail if no handler provided
+  if(!getHandler) {
+    console.log('postfix: ERROR, no handler provided!')
+    process.exit(1)
+  }
 
-  return (client) => {
-    clientId += 1
+  return client => {
+    // Next client
+    clientId++
+
     const service = `${serviceName}(${clientId})`
     console.log(`${service}: Postfix client connected`)
-
     client.on('end', () => console.log(`${service}: Postfix client disconnected`))
 
     carrier.carry(client, null, 'ascii')
-      .on('line', async (line) => {
+      .on('line', line => {
         // Make sure this is a get request and that it has a parameter
-        const tokens = line.toLowerCase().split(' ')
-        if(tokens.length === 2 && tokens[0] === 'get') {
+        line = line.toLowerCase()
+        const tokens = line.split(' ')
+        if(tokens.length == 2 && tokens[0] == 'get') {
           // Unescape the parameter value
-          const reqval = unescape(tokens[1])
+          const reqval = decodeURIComponent(tokens[1])
           console.log(`${service}: Got get request ${reqval}`)
 
           // Have the subclass do its lookup
-          try {
-            const respval = await getHandler(reqval)
-            console.log(`${service}: responding with ${respval}`)
-            client.write(`200 ${respval}\n`)
-          } catch(err) {
-            console.log(`${service}: responding with Not Found`)
-            client.write('500 Not Found\n')
-          }
+          getHandler(reqval).then(respval => {
+            if(respval) {
+              console.log(`${service}: responding with ${respval}`)
+              client.write(`200 ${respval}\n`)
+            } else {
+              console.log(`${service}: responding with Not Found`)
+              client.write('500 Not Found\n')
+            }
+          })
         } else {
-          console.log(`${service}: got an unknown request line (${line})`)
+          console.log(`${service}: got an unknow request line (${line})`)
           client.write('400 Unknown or unsupported request type\n')
         }
+      })
+      .on('end', function() {
+        client.end()
       })
   }
 }
 
-const domainHandler = () => postfixHandler('domain', (domain) => db.get(domain).then(() => 'OK'))
-const mailboxHandler = () => postfixHandler('mailbox', (username) => db.get(username).then(() => username))
-const aliasHandler = () => postfixHandler('alias', (alias) => db.get(`alias-${alias}`).then((body) => body.target))
+// Postfix handler specialized for finding domains
+const domainHandler = () =>
+  postfixHandler('domain', (domain, callback) =>
+    db.get(domain)
+      .then(body => 'OK')
+      .catch(err => {
+        console.log(`domain lookup err ${err.message}`)
+        return ''
+      })
+  )
 
+// Postfix handler specialized for finding mailboxes
+const mailboxHandler = () =>
+  postfixHandler('mailbox', (username, callback) =>
+    db.get(username)
+      .then(body => username)
+      .catch(err => {
+        console.log(`mailbox lookup err ${err.message}`)
+        return ''
+      })
+  )
+
+// Postfix handler specialized for finding aliases
+const aliasHandler = () =>
+  postfixHandler('alias', (alias, callback) =>
+    db.get(`alias-${alias}`)
+      .then(body => body.target)
+      .catch(err => {
+        console.log(`alias lookup err ${err.message}`)
+        return ''
+      })
+  )
+
+// This processes escaped values in stored dovecot data
+const dovecotEscape = (str) => {
+  return str.replace(/\0o001/g,'\0o0011')
+    .replace(/\n/g,'\0o001n')
+    .replace(/\t/g, '\0o001t')
+}
+
+// Handles authentication requests in the dovecot auth format
 const dovecotAuthHandler = () => {
   // Dovecot makes one request per connection
   const CMD_HELLO = 'H'
   const CMD_LOOKUP = 'L'
 
-  const dovecotEscape = (str) => str
-    .replace(/\x01/g, '\x011') // eslint-disable-line no-control-regex
-    .replace(/\n/g, '\x01n')
-    .replace(/\t/g, '\x01t')
+  const vars = {}
 
-  return (client) => {
+  return async client => {
     console.log('Dovecot: client connected')
-    client.on('end', () => console.log('Dovecot: client disconnected'))
-
-    const vars = {}
+    client.on('end', () => { console.log('Dovecot: client disconnected') })
 
     carrier.carry(client, null, 'ascii')
-      .on('line', async (line) => {
+      .on('line', line => {
         console.log(`Dovecot: got request line (${line})`)
         const cmd = line[0]
 
         switch(cmd) {
-        case CMD_HELLO: {
-          const vals = line.substring(1).split('\t')
-          vars.user = vals[3]
-          vars.table = vals[4]
-        }
-          break
+          case CMD_HELLO:
+            // Each connection is for a specific table and user, cache these values on hello
+            const vals = line.substring(1).split('\t')
+            vars.table = vals[4]
+            vars.user = vals[3]
+            break
 
-        case CMD_LOOKUP:
-          switch(vars.table) {
-          case 'auth':
-            vars.user = line.split('/')[1]
-            console.log(`Dovecot: looking up auth for ${vars.user}`)
-            try {
-              const user = await db.get(vars.user)
-              console.log(`Dovecot: Found auth entry in db for ${user._id}`)
-              client.write('O')
-              client.write(JSON.stringify({ password: user.password }))
-              client.write('\n')
-            } catch(err) {
-              console.log('Dovecot: responding with auth Not Found')
-              client.write('N\n')
+          case CMD_LOOKUP:
+            switch(vars.table) {
+              case 'auth':
+                vars.user = line.split('/')[1]
+                console.log(`Dovecot: looking up auth for ${vars.user}`)
+                db.get(vars.user)
+                  .then(body => {
+                    console.log(`Dovecot: Found entry in db for ${body._id}`)
+                    client.write('O')
+                    client.write(JSON.stringify({ password : body.password }))
+                    client.write('\n')
+                  })
+                  .catch(err => {
+                    console.log('Dovecot: responding with Not Found')
+                    client.write('N\n')
+                  })
+                break
+
+              case 'sieve':
+                console.log(`Dovecot: looking up sieve for ${vars.user}`)
+                const paths = line.split('/')
+                if(paths[2] === 'name') {
+                  // Dovecot caches the compiled script based on the ID we return
+                  // so let's return a composite key based on the _id and _rev of the script
+                  db.get(vars.user)
+                    .then(body =>
+                      db.get(body.sieve[paths[3]])
+                        .then(body => {
+                          console.log(`Dovecot: Found entry in db for ${body._id}`)
+                          client.write('O')
+                          client.write(`${body._id}+${body._rev}`)
+                          client.write('\n')
+                        })
+                        .catch(err => {
+                          console.log('Dovecot: could not find the script document for the user')
+                          client.write('N\n')
+                        })
+                    ).catch(err => {
+                      console.log('Dovecot: could not find script with that name for user')
+                      client.write('N\n')
+                    })
+                } else if(paths[2] === 'data') {
+                  db.get(paths[3].split('+')[0])
+                    .then(body => {
+                      console.log(`Dovecot: Found entry in db for ${body._id}`)
+                      client.write('O')
+                      client.write(dovecotEscape(body.script))
+                      client.write('\n')
+                    })
+                    .catch(err => {
+                      console.log('Dovecot: could not find script with that id')
+                      client.write('N\n')
+                    })
+                }
+                break
             }
             break
-
-          case 'sieve': {
-            console.log(`Dovecot: looking up sieve for ${vars.user}`)
-            const paths = line.split('/')
-            switch(paths[2]) {
-            case 'name':
-              // Dovecot caches the compiled script based on the ID we return
-              // so let's return a composite key based on the _id and _rev of the script
-              try {
-                const user = await db.get(vars.user)
-                const sieve = await db.get(user.sieve[paths[3]])
-
-                console.log(`Dovecot: Found sieve entry in db for ${sieve._id}:${sieve._rev}`)
-
-                client.write('O')
-                client.write(`${sieve._id}+${sieve._rev}`)
-                client.write('\n')
-              } catch(err) {
-                console.log(`Dovecot: could not find the sieve script document reference for the user ${vars.user}`)
-                client.write('N\n')
-              }
-              break
-
-            case 'data':
-              try {
-                const data = await db.get(paths[3].split('+')[0])
-
-                console.log(`Dovecot: Found data entry in db for ${data._id}`)
-
-                client.write('O')
-                client.write(dovecotEscape(data.script))
-                client.write('\n')
-              } catch(err) {
-                console.log(`Dovecot: could not find script data with id ${paths[3]}`)
-                client.write('N\n')
-              }
-              break
-
-            default:
-              console.log(`Dovecot: Unknown sieve operation: ${paths[2]}`)
-              client.write('N\n')
-              break
-            }
-          }
-            break
-
-          default:
-            console.log(`Dovecot: Unknown table: ${vars.table}`)
-            client.write('N\n')
-            break
-          }
-          break
-
-        default:
-          console.log(`Dovecot: Unknown command, disconnecting: ${cmd}`)
-          client.end()
-          break
         }
+      })
+      .on('end', function() {
+        client.end()
       })
   }
 }
 
-net.createServer(domainHandler()).listen(40571, '0.0.0.0', () => console.log('domain handler listening on 40571'))
-net.createServer(mailboxHandler()).listen(40572, '0.0.0.0', () => console.log('mailbox handler listening on 40572'))
-net.createServer(aliasHandler()).listen(40573, '0.0.0.0', () => console.log('alias handler listening on 40573'))
-net.createServer(dovecotAuthHandler()).listen(40574, '0.0.0.0', () => console.log('dovecot auth handler listening on 40574'))
+console.log('Creating TCP listeners')
+net.createServer(domainHandler())
+  .on('error', err => console.log(err))
+  .listen(40571)
+net.createServer(mailboxHandler())
+  .on('error', err => console.log(err))
+  .listen(40572)
+net.createServer(aliasHandler())
+  .on('error', err => console.log(err))
+  .listen(40573)
+
+console.log('Dovecot: creating Unix listener')
+const dovecotSocket = '/var/run/couchmail/dovecot-auth.sock'
+const dovecotServer = net.createServer(dovecotAuthHandler())
+  .on('error', err => console.log(err))
+const startDovecotServer = () => {
+  console.log('Dovecot: starting handler')
+  const oldumask = process.umask(0o000)
+  dovecotServer.listen(dovecotSocket, () => { process.umask(oldumask) })
+}
+
+// See if the socket file already exists
+console.log('Dovecot: check for socket')
+if(fs.existsSync(dovecotSocket)) {
+  // Attempt to connect to a server process that is possibly already listening
+  console.log('Dovecot: auth socket already exists')
+  new net.Socket()
+    .on('error', function(e) {
+      console.log(`Dovecot: could not connect to socket ${e.code}`)
+      if (e.code == 'ECONNREFUSED') {
+        // No other server listening so delete the file
+        console.log('Dovecot: deleting unused server socket')
+        fs.unlinkSync(dovecotSocket)
+        startDovecotServer()
+      } else {
+        console.log('Some error besides conn refused happened when trying to check for an existing daemon', e)
+        process.exit(1)
+      }
+    })
+    .connect({path: dovecotSocket}, function() {
+      // If a connection is successful, another instance is already running
+      console.log('Server already running, giving up...')
+      process.exit(1)
+    })
+} else {
+  startDovecotServer()
+}
+
